da387c14e5e5b5d2a411858594c222d858dcf858
raj <rjvsonawala@gmail.com>
2018-09-21 13:24:09 +0530

    Landing Activity Finished with recycler View

--- app/src/main/java/com/knoxpo/rajivsonawala/easytow_officer/Fragment/LandingFragment.java
+++ app/src/main/java/com/knoxpo/rajivsonawala/easytow_officer/Fragment/LandingFragment.java
@@ -9,13 +9,20 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,6 +35,8 @@ public class LandingFragment extends Fragment {
     private TextView mPhoneTV;
     private Button mSignOutBtn;
     private ArrayList<String> mVehicle=new ArrayList<String>();
+    private RecyclerView mRecyclerView;
+    private DetailsAdapter mAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,6 +48,13 @@ public class LandingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_landing, container, false);
+
+        mRecyclerView=v.findViewById(R.id.landing_recycler_view);
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new DetailsAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
        // init(v);
 
 /*
@@ -57,6 +73,8 @@ public class LandingFragment extends Fragment {
 
         return v;
     }
+
+
 
 
 
@@ -95,6 +113,8 @@ public class LandingFragment extends Fragment {
                     String tempString=data.getExtras().getString("result");
                     Toast.makeText(getContext(),"vehicle Number:"+data.getExtras().getString("result"),Toast.LENGTH_LONG).show();
                     mVehicle.add(tempString);
+                    printArrayList();
+                    mAdapter.notifyDataSetChanged();
                 }
 
             }
@@ -109,5 +129,75 @@ public class LandingFragment extends Fragment {
         mPhoneTV = v.findViewById(R.id.tv_phone);
         mSignOutBtn = v.findViewById(R.id.btn_sign_out);
     }
-*/
+    */
+
+    private class DetailsAdapter extends RecyclerView.Adapter<DetailVH>{
+
+        private LayoutInflater mInflater;
+
+        public DetailsAdapter(){
+
+            mInflater= LayoutInflater.from(getActivity());
+
+        }
+
+
+        @NonNull
+        @Override
+        public DetailVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v = mInflater.inflate(R.layout.fragment_item_view, parent, false);
+
+            return new DetailVH(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull DetailVH holder, int position) {
+
+            holder.bind(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mVehicle.size();
+        }
+    }
+
+    private class DetailVH extends RecyclerView.ViewHolder{
+
+        TextView mIndexNumber;
+        TextView mDetails;
+        ImageButton mDelete;
+        ImageButton mRight;
+
+
+        public DetailVH(View itemView) {
+            super(itemView);
+            mIndexNumber=itemView.findViewById(R.id.entry_no);
+            mDetails=itemView.findViewById(R.id.vehicle_details);
+            mDelete=itemView.findViewById(R.id.entry_delete_button);
+            mRight=itemView.findViewById(R.id.true_button);
+
+        }
+
+
+        public void bind(int position) {
+
+            mIndexNumber.setText("1");
+            mDetails.setText(mVehicle.get(position));
+
+        }
+    }
+
+    public void printArrayList(){
+
+        for(int i=0;i<mVehicle.size();i++){
+            Log.d("temp", ": "+mVehicle.get(i));
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("temp", "onDestroy: ");
+    }
 }
